83a433267e4d4c90cdf646d3e5b373d61e51a040
44kia244 <ch.kiarttipum_st@tni.ac.th>
2015-11-30 21:31:26 +0700

    Correct View Loader Call Style to Static
    $Loader->Load()
    TO
    MWF_ViewLoader::Load()

--- MWFramework/index.php
+++ MWFramework/index.php
@@ -18,7 +18,5 @@
 ?>
 <?php
 	require_once("core/ClassLoader.php");
-	
-	$Loader = new MWF_ViewLoader();
-	$Loader->Load($_GET["mod"], $_GET["view"]);
+	MWF_ViewLoader::Load($_GET["mod"], $_GET["view"]);
 ?>
