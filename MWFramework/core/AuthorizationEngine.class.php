3e2c4e6539b72a7b0ab144f05d09406967f829ad
44kia244 <ch.kiarttipum_st@tni.ac.th>
2015-12-07 13:18:22 +0700

    Initialize AuthorizationEngine
    - Initial Code for AuthorizationEngine

--- MWFramework/core/AuthorizationEngine.class.php
+++ MWFramework/core/AuthorizationEngine.class.php
@@ -18,8 +18,47 @@
 ?>
 <?php
 	class AuthorizationEngine {
+		private $DB;
+		
 		public function __construct() {
-			
+			$this->DB = new DBengine();
+		}
+		
+		public function getGroupList() {
+			return $this->DB->query("SELECT G_ID, G_NAME FROM GROUPS");
+		}
+		
+		public function getGroupInfo($G_ID) {
+			return $this->DB->query("SELECT G_ID, G_NAME FROM GROUPS WHERE G_ID = ?",
+				array(
+					array("i", $G_ID)
+				)
+			);
+		}
+		
+		public function getGroupPermList($G_ID) {
+			return $this->DB->query("SELECT P_ID, P_DESC FROM GROUPS G JOIN PERMISSIONS P ON (P.P_ID = G.G_ID) WHERE G.G_ID = ?",
+				array(
+					array("i", $G_ID)
+				)
+			);
+		}
+		
+		public function isHasGroup($G_ID) {
+			return count($this->DB->query("SELECT G_ID FROM GROUPS WHERE G_ID = ?",
+				array(
+					array("i", $G_ID)
+				)
+			)) == 1;
+		}
+		
+		public function isHasPermission($G_ID, $P_ID) {
+			return count($this->DB->query("SELECT P_ID, P_DESC FROM GROUPS G JOIN PERMISSIONS P ON (P.P_ID = G.G_ID) WHERE G.G_ID = ? AND G.P_ID = ?",
+				array(
+					array("i", $G_ID),
+					array("i", $P_ID)
+				)
+			)) == 1;
 		}
 	}
 ?>
