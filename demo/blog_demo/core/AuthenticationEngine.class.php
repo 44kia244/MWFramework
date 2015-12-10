9b198ae86269d9f341f45fc55f1e585097353739
Piyawat Prasitvarakul <shirogaku@outlook.com>
2015-12-10 16:30:50 +0700

    Added Blog Demo mod_login

---
+++ demo/blog_demo/core/AuthenticationEngine.class.php
@@ -0,0 +1,98 @@
+<?php
+	/****************************************************************************
+	*	This file is part of MWFramework.                                       *
+	*	                                                                        *
+	*	MWFramework is free software: you can redistribute it and/or modify     *
+	*	it under the terms of the GNU General Public License as published by    *
+	*	the Free Software Foundation, either version 3 of the License, or       *
+	*	(at your option) any later version.                                     *
+	*	                                                                        *
+	*	MWFramework is distributed in the hope that it will be useful,          *
+	*	but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+	*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+	*	GNU General Public License for more details.                            *
+	*	                                                                        *
+	*	You should have received a copy of the GNU General Public License       *
+	*	along with MWFramework.  If not, see <http://www.gnu.org/licenses/>.    *
+	****************************************************************************/
+?>
+<?php
+	class AuthenticationEngine {
+		private $DB;
+		private $login;
+		
+		public function __construct() {
+			$this->DB = new DBengine();
+			session_start();
+				if(!empty($_SESSION["login"])) $this->session_sync();
+				else session_destroy();
+		}
+		
+		private function check_login($username, $password) {
+			$search = $this->getuserdata($username);
+			
+			if(count($search) === 1) {
+				if($search["username"] === $this->mkusername($username) && $search["password"] === $this->mkpassword($password)) return TRUE;
+			} else return FALSE;
+		}
+		
+		public function login($username, $password) {
+			if($this->check_login($username, $password)) {
+				session_start();
+				$_SESSION["login"] = hash("SHA256", "#" . $username . $this->mkpassword($username, $password) . $username . "#");
+				$this->session_sync();
+				return TRUE;
+			} else return FALSE;
+		}
+		
+		public function logout() {
+			$_SESSION["login"] = NULL;
+			$this->session_sync();
+			session_destroy();
+		}
+		
+		public function register($username, $password) {
+			if(count($this->getuserdata($username)) >= 1) return FALSE;
+			
+			$result = $this->DB->query("INSERT INTO `users` (`username`, `password`) VALUES (?, ?)",
+				array(
+					array("s", $this->mkusername($username)),
+					array("s", $this->mkpassword($username, $password))
+				)
+			);
+			
+			return $result;
+		}
+		
+		public function getuserdata($username) {
+			return $this->DB->query("SELECT * FROM `users` WHERE `username` = ?",
+				array(
+					array("s", $this->mkusername($username))
+				)
+			);
+		}
+		
+		public function changepassword($username, $password, $newpassword) {
+			if($this->check_login($username, $password)) {
+				return $this->DB->query("UPDATE `users` SET `password` = ? WHERE `username` = ?",
+					array(
+						array("s", $this->mkusername($username)),
+						array("s", $this->mkpassword($username, $newpassword))
+					)
+				);
+			} return FALSE;
+		}
+		
+		private function mkusername($username) {
+			return $username;
+		}
+		
+		private function mkpass($username, $password) {
+			return hash("SHA512", $this->mkusername($username) . $password);
+		}
+		
+		private function session_sync() {
+			$this->login = $_SESSION["login"];
+		}
+	}
+?>
---
+++ demo/blog_demo/core/AuthorizationEngine.class.php
@@ -0,0 +1,92 @@
+<?php
+	/****************************************************************************
+	*	This file is part of MWFramework.                                       *
+	*	                                                                        *
+	*	MWFramework is free software: you can redistribute it and/or modify     *
+	*	it under the terms of the GNU General Public License as published by    *
+	*	the Free Software Foundation, either version 3 of the License, or       *
+	*	(at your option) any later version.                                     *
+	*	                                                                        *
+	*	MWFramework is distributed in the hope that it will be useful,          *
+	*	but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+	*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+	*	GNU General Public License for more details.                            *
+	*	                                                                        *
+	*	You should have received a copy of the GNU General Public License       *
+	*	along with MWFramework.  If not, see <http://www.gnu.org/licenses/>.    *
+	****************************************************************************/
+?>
+<?php
+	class AuthorizationEngine {
+		private $DB;
+		
+		public function __construct() {
+			$this->DB = new DBengine();
+		}
+		
+		public function getGroupList() {
+			return $this->DB->query("SELECT G_ID, G_NAME FROM GROUPS");
+		}
+		
+		public function getGroupPermList($G_ID) {
+			return $this->DB->query("SELECT P_ID, P_DESC FROM GROUPS G JOIN PERMISSIONS P ON (P.P_ID = G.G_ID) WHERE G.G_ID = ?",
+				array(
+					array("i", $G_ID)
+				)
+			);
+		}
+		
+		public function getGroupInfo($G_ID) {
+			return $this->DB->query("SELECT G_ID, G_NAME FROM GROUPS WHERE G_ID = ?",
+				array(
+					array("i", $G_ID)
+				)
+			);
+		}
+		
+		public function getPermissionInfo($P_ID) {
+			return $this->DB->query("SELECT P_ID, P_DESC FROM PERMISSIONS WHERE P_ID = ?",
+				array(
+					array("i", $P_ID)
+				)
+			);
+		}
+		
+		public function isAuthorized($G_ID, $P_ID) {
+			$T = $this->DB->query("SELECT COUNT(*) FROM AUTHORIZATION A JOIN PERMISSIONS P ON (P.P_ID = A.P_ID) WHERE A.G_ID = ? AND G.P_ID = ?",
+				array(
+					array("i", $G_ID),
+					array("i", $P_ID)
+				)
+			);
+			
+			return $T[0][0] == 1;
+		}
+		
+		public function addGroup($G_NAME) {
+			return $this->DB->query("INSERT INTO GROUPS (G_NAME) VALUES (?)",
+				array(
+					array("s", $G_NAME)
+				)
+			);
+		}
+		
+		public function addPermission($P_DESC) {
+			return $this->DB->query("INSERT INTO PERMISSIONS (P_DESC) VALUES (?)",
+				array(
+					array("s", $P_DESC)
+				)
+			);
+		}
+		
+		public function authorize($G_ID, $P_ID) {
+			if(!($this->isHasGroup($G_ID) && $this->isHasPermission($P_ID))) return FALSE;
+			return $this->DB->query("INSERT INTO AUTHORIZATION (G_ID, P_ID) VALUES (?, ?)",
+				array(
+					array("i", $G_ID),
+					array("i", $P_ID)
+				)
+			);
+		}
+	}
+?>
