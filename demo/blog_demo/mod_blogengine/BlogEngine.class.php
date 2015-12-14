8d2155e1f0136246f3e92b68f07586632cf35ade
44kia244 <ch.kiarttipum_st@tni.ac.th>
2015-12-14 11:06:28 +0700

    UPDATE
    - Added BlogEngine Class Description

--- demo/blog_demo/mod_blogengine/BlogEngine.class.php
+++ demo/blog_demo/mod_blogengine/BlogEngine.class.php
@@ -1,4 +1,11 @@
 <?php
+	/*
+	*  Blog Content Controller Class
+	*    - DBengine Required
+	*    - AuthenticationEngine Required
+	*    - AuthorizationEngine Required
+	*/
+	
 	class BlogEngine{
 		$query = NULL;
 	}
